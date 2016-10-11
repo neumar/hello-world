0750b3a15aa08898f1cf9ff93c0628f171802efe
neumar <neumar@gmail.com>
2016-10-11 01:16:44 -0300

    Add files via upload

---
+++ EchoServer.java
@@ -0,0 +1,56 @@
+import java.net.*; 
+import java.io.*; 
+
+public class EchoServer 
+{ 
+ public static void main(String[] args) throws IOException 
+   { 
+    ServerSocket serverSocket = null;
+		 int porta = 10007;
+
+    try { 
+         serverSocket = new ServerSocket(porta); 
+        } 
+    catch (IOException e) 
+        { 
+         System.err.println("Could not listen on port: " + porta); 
+         System.exit(1); 
+        } 
+
+    Socket clientSocket = null; 
+    System.out.println ("Waiting for connection.....");
+
+    try { 
+         clientSocket = serverSocket.accept(); 
+        } 
+    catch (IOException e) 
+        { 
+         System.err.println("Accept failed."); 
+         System.exit(1); 
+        } 
+
+    System.out.println ("Connection successful");
+    System.out.println ("Waiting for input.....");
+
+    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
+                                      true); 
+    BufferedReader in = new BufferedReader( 
+            new InputStreamReader( clientSocket.getInputStream())); 
+
+    String inputLine; 
+
+    while ((inputLine = in.readLine()) != null) 
+        { 
+         System.out.println ("Server: " + inputLine); 
+         out.println(inputLine.toUpperCase()); 
+
+         if (inputLine.equals("Bye.")) 
+             break; 
+        } 
+
+    out.close(); 
+    in.close(); 
+    clientSocket.close(); 
+    serverSocket.close(); 
+   } 
+} 
